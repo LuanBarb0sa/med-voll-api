0e0bb8c6f2418fbc2649f7dfeef1bb4e551faf22
LuanBarb0sa <luanlucas.r10@hotmail.com>
2023-04-11 09:06:59 -0300

    Controller das classes Medico/Paciente

---
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,27 @@
+package med.voll.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.transaction.Transactional;
+import med.voll.api.dto.DadosCadastroMedico;
+import med.voll.api.model.Medico;
+import med.voll.api.repository.MedicoRepository;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+	
+	@Autowired
+	private MedicoRepository repository;
+	
+	@PostMapping
+	@Transactional
+	public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+		repository.save(new Medico(dados));
+	}
+	
+}
