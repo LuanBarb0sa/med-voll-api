0b5e4ffbf6826d0dba93c959752aa05974d43ca8
LuanBarb0sa <luanlucas.r10@hotmail.com>
2023-04-11 11:10:11 -0300

    Implementado método cadastro paciente

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,10 +1,37 @@
 package med.voll.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.dto.DadosCadastroPaciente;
+import med.voll.api.model.Paciente;
+import med.voll.api.repository.PacienteRepository;
 
 @RestController
 @RequestMapping("/pacientes")
 public class PacienteController {
+
+	@Autowired
+	private PacienteRepository repository;
+
+	@PostMapping
+	@Transactional
+	public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+
+		try {
+
+			repository.save(new Paciente(dados));
+
+		} catch (Exception e) {
+
+		}
+
+	}
 
 }
