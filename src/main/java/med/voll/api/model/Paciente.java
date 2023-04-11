b76c209e599eb136abd8cbfef954440bbb811b36
LuanBarb0sa <luanlucas.r10@hotmail.com>
2023-04-11 11:11:56 -0300

    Ajuste nas entidades Medico/Paciente

--- src/main/java/med/voll/api/model/Paciente.java
+++ src/main/java/med/voll/api/model/Paciente.java
@@ -1,15 +1,27 @@
 package med.voll.api.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
+import med.voll.api.dto.DadosCadastroPaciente;
 
 @Data
-@Entity(name = "tb_paciente")
+@Table(name = "tb_paciente")
+@Entity(name = "Paciente")
 public class Paciente {
+	
+	public Paciente(DadosCadastroPaciente dados) {
+		this.nome = dados.nome();
+		this.email = dados.email();
+		this.telefone = dados.telefone();
+		this.cpf = dados.cpf();
+		this.endereco = new Endereco(dados.endereco());
+	}
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,5 +30,17 @@ public class Paciente {
 
 	@Column(name = "nome")
 	private String nome;
-
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "telefone")
+	private String telefone;
+	
+	@Column(name = "cpf")
+	private String cpf;
+	
+	@Embedded
+	private Endereco endereco;
+	
 }
