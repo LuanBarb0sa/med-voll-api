0fdcfe6bfd9740f063c76f4503d5612ebf7eb2df
LuanBarb0sa <luanlucas.r10@hotmail.com>
2023-04-11 09:03:17 -0300

    Ajuste na classe Medico Model

--- src/main/java/med/voll/api/model/Medico.java
+++ src/main/java/med/voll/api/model/Medico.java
@@ -1,22 +1,53 @@
 package med.voll.api.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
+import med.voll.api.dto.DadosCadastroMedico;
+import med.voll.api.enums.Especialidade;
 
 @Data
-@Entity(name = "tb_medico")
+@Table(name = "tb_medicos")
+@Entity(name = "Medico")
 public class Medico {
+
+	public Medico(DadosCadastroMedico dados) {
+		this.nome = dados.nome();
+		this.email = dados.email();
+		this.crm = dados.crm();
+		this.especialidade = dados.especialidade();
+		this.endereco = new Endereco(dados.endereco());
+	}
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
-	private int id;
+	private long id;
 
 	@Column(name = "nome")
 	private String nome;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "telefone")
+	private String telefone;
+
+	@Column(name = "crm")
+	private String crm;
+
+	@Column(name = "especialidade")
+	@Enumerated(EnumType.STRING)
+	private Especialidade especialidade;
+
+	@Embedded
+	private Endereco endereco;
 
 }
