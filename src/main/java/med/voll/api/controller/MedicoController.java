4f0ba59aba20062d22ac5b3a9592f29086c14ac0
LuanBarb0sa <luanlucas.r10@hotmail.com>
2023-04-11 10:03:42 -0300

    Ajuste no método cadastrar Medico

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import med.voll.api.dto.DadosCadastroMedico;
 import med.voll.api.model.Medico;
 import med.voll.api.repository.MedicoRepository;
@@ -14,14 +15,21 @@ import med.voll.api.repository.MedicoRepository;
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
-	
+
 	@Autowired
 	private MedicoRepository repository;
-	
+
 	@PostMapping
 	@Transactional
-	public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-		repository.save(new Medico(dados));
+	public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
+
+		try {
+
+			repository.save(new Medico(dados));
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
-	
+
 }
