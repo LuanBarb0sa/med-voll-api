d09af0b235219177cc62a52482ad081d178af774
LuanBarb0sa <luanlucas.r10@hotmail.com>
2023-04-12 00:07:08 -0300

    Ajuste DTO Medico/Paciente

--- src/main/java/med/voll/api/dto/DadosListagemMedico.java
+++ src/main/java/med/voll/api/dto/DadosListagemMedico.java
@@ -3,11 +3,12 @@ package med.voll.api.dto;
 import med.voll.api.enums.Especialidade;
 import med.voll.api.model.Medico;
 
-public record DadosListagemMedico(String nome, String email, String crm, Especialidade especialidade) {
+public record DadosListagemMedico(Long id,String nome, String email, String crm, Especialidade especialidade) {
 
 	
 	public DadosListagemMedico(Medico medico) {
-		this(medico.getNome(),
+		this(	medico.getId(),
+				medico.getNome(),
 				medico.getEmail(),
 				medico.getCrm(),
 				medico.getEspecialidade());
--- src/main/java/med/voll/api/dto/DadosListagemPaciente.java
+++ src/main/java/med/voll/api/dto/DadosListagemPaciente.java
@@ -2,10 +2,12 @@ package med.voll.api.dto;
 
 import med.voll.api.model.Paciente;
 
-public record DadosListagemPaciente(String nome, String email, String cpf) {
+public record DadosListagemPaciente(Long id, String nome, String email, String cpf) {
 
 	public DadosListagemPaciente(Paciente paciente) {
-		this(paciente.getNome(),
+		this(
+			 paciente.getId(),	
+			 paciente.getNome(),
 			 paciente.getEmail(),
 			 paciente.getCpf());
 	}
