a727036aaa955deaa98ac8a486829caa21cb4546
LuanBarb0sa <luanlucas.r10@hotmail.com>
2023-04-12 00:06:26 -0300

    Implementando inativação Medico/Paciente

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,18 +1,23 @@
 package med.voll.api.controller;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voll.api.dto.DadosAtualizacaoMedico;
 import med.voll.api.dto.DadosCadastroMedico;
 import med.voll.api.dto.DadosListagemMedico;
 import med.voll.api.model.Medico;
@@ -32,8 +37,22 @@ public class MedicoController {
 	}
 
 	@GetMapping
-	public Page<DadosListagemMedico> listar(Pageable paginacao) {
-		return repository.findAll(paginacao).map(DadosListagemMedico::new);
+	public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, page = 0, sort = {"nome"}) Pageable paginacao) {
+		return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
 	}
-
+	
+	@PutMapping
+	@Transactional
+	public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+		var medico = repository.getReferenceById(dados.id());
+		medico.atualizarInformacoes(dados);
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public void excluir(@PathVariable Long id) {
+		var medico = repository.getReferenceById(id);
+		medico.inativar();
+	}
+	
 }
--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,19 +1,19 @@
 package med.voll.api.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voll.api.dto.DadosAtualizacaoPaciente;
 import med.voll.api.dto.DadosCadastroPaciente;
 import med.voll.api.dto.DadosListagemPaciente;
 import med.voll.api.model.Paciente;
@@ -35,8 +35,14 @@ public class PacienteController {
 	}
 	
 	@GetMapping
-	public Page<DadosListagemPaciente> listar(Pageable paginacao) {
+	public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10, page = 0, sort = {"nome"}) Pageable paginacao) {
 		return repository.findAll(paginacao).map(DadosListagemPaciente::new);
+	}
+	
+	@PutMapping
+	public void atualizar(@RequestBody @Valid DadosAtualizacaoPaciente dados) {
+		var paciente = repository.getReferenceById(dados.id());
+		paciente.atualizarInformacoes(dados);
 	}
 	
 }
