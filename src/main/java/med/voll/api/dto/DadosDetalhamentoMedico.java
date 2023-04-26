d5a25225f68c2adae54496b151b4bcebb279ecc5
LuanBarb0sa <luanlucas.r10@hotmail.com>
2023-04-26 16:52:01 -0300

    Correção DTO DetalhamentoMedico

--- src/main/java/med/voll/api/dto/DadosDetalhamentoMedico.java
+++ src/main/java/med/voll/api/dto/DadosDetalhamentoMedico.java
@@ -4,10 +4,12 @@ import med.voll.api.enums.Especialidade;
 import med.voll.api.model.Endereco;
 import med.voll.api.model.Medico;
 
-public record DadosDetalhamentoMedico(Long id, String nome, String email, String crm,String telefone, Especialidade especialidade, Endereco endereco) {
+public record DadosDetalhamentoMedico(Long id, String nome, String email, String crm, String telefone,
+		Especialidade especialidade, Endereco endereco) {
 
 	public DadosDetalhamentoMedico(Medico medico) {
-		this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getTelefone(), medico.getEspecialidade(), medico.getEndereco());
+		this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getTelefone(),
+				medico.getEspecialidade(), medico.getEndereco());
 	}
-	
+
 }
