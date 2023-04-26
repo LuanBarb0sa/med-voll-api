3b6719de9c8e8e8fb758557e333be3ac3cc05d27
LuanBarb0sa <luanlucas.r10@hotmail.com>
2023-04-26 16:50:47 -0300

    RECORD para detalhamento de Paciente

---
+++ src/main/java/med/voll/api/dto/DadosDetalhamentoPaciente.java
@@ -0,0 +1,16 @@
+package med.voll.api.dto;
+
+import med.voll.api.model.Endereco;
+import med.voll.api.model.Paciente;
+
+public record DadosDetalhamentoPaciente(String nome, String email, String telefone, String cpf, Endereco endereco) {
+
+	public DadosDetalhamentoPaciente(Paciente paciente) {
+		this(paciente.getNome(),
+		paciente.getEmail(),
+		paciente.getTelefone(),
+		paciente.getCpf(),
+		paciente.getEndereco());
+	}
+
+}
